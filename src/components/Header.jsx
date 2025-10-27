347dd09d78dfc07e94c3be4553c13a61f9f934ae
Baraiya Nikhil <baraiyanikhil593@gmail.com>
2025-10-27 22:50:24 +0530

    created the Header.jsx and Home.jsx and modified the App.jsx for Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function Header() {
+  const [isLight, setIsLight] = useState(true);
+
+  return (
+    <header className="dark:bg-black/5 w-full h-24 flex justify-center items-center sticky top-0">
+      <nav className="dark:bg-white w-1/4 bg-black/5 px-10 py-2.5 rounded-3xl">
+        <ul className="w-full flex justify-around font-semibold text-2xl font-">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Learn</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+        </ul>
+      </nav>
+      <button className="fixed top-8 right-20" id="theam_btn" onClick={() => setIsLight((i) => !i)}>
+        {isLight ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+          >
+            <path d="M 11 0 L 11 3 L 13 3 L 13 0 L 11 0 z M 4.2226562 2.8085938 L 2.8085938 4.2226562 L 4.9296875 6.34375 L 6.34375 4.9296875 L 4.2226562 2.8085938 z M 19.777344 2.8085938 L 17.65625 4.9296875 L 19.070312 6.34375 L 21.191406 4.2226562 L 19.777344 2.8085938 z M 12 5 C 8.1458514 5 5 8.1458514 5 12 C 5 15.854149 8.1458514 19 12 19 C 15.854149 19 19 15.854149 19 12 C 19 8.1458514 15.854149 5 12 5 z M 12 7 C 14.773268 7 17 9.2267316 17 12 C 17 14.773268 14.773268 17 12 17 C 9.2267316 17 7 14.773268 7 12 C 7 9.2267316 9.2267316 7 12 7 z M 0 11 L 0 13 L 3 13 L 3 11 L 0 11 z M 21 11 L 21 13 L 24 13 L 24 11 L 21 11 z M 4.9296875 17.65625 L 2.8085938 19.777344 L 4.2226562 21.191406 L 6.34375 19.070312 L 4.9296875 17.65625 z M 19.070312 17.65625 L 17.65625 19.070312 L 19.777344 21.191406 L 21.191406 19.777344 L 19.070312 17.65625 z M 11 21 L 11 24 L 13 24 L 13 21 L 11 21 z"></path>
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 -960 960 960"
+          >
+            <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+          </svg>
+        )}
+      </button>
+    </header>
+  );
+}
