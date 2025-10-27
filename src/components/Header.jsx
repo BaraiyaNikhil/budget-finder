c7d5c0fb8375c745d6ac3bced6c446b0090750cb
Baraiya Nikhil <baraiyanikhil593@gmail.com>
2025-10-27 22:57:39 +0530

    fixed the padding bug of nav and updated index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="dark:bg-black/5 w-full h-24 flex justify-center items-center sticky top-0">
       <nav className="dark:bg-white w-1/4 bg-black/5 px-10 py-2.5 rounded-3xl">
-        <ul className="w-full flex justify-around font-semibold text-2xl font-">
+        <ul className="w-full flex justify-around font-semibold text-2xl">
           <li>
             <a href="">Home</a>
           </li>
